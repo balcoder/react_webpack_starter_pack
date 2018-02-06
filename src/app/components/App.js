e6e1b18b92055216c85e368a87a994b2a1179ffc
Des Barrett <des.barrett@gmail.com>
2018-02-06 20:34:21 +0000

    move app.js from index to components

---
+++ src/app/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+      <div className="container">
+        <h1 className="text-center">React Webpack Starter Pack</h1>
+      </div>
+    );
+  }
+}
